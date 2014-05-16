ab8ec89e53c1e53f27c786a604d5b7e3e20626a4
Jared M Daugherty <jaredmdaugherty@gmail.com>
2014-05-16 18:02:53 -0500

    Seed decks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,9 @@
-100.times { Card.create( question: Faker::Lorem.word, answer: Faker::Lorem.word)}
+latin = Deck.create(name: Faker::Lorem.word)
+10.times { Card.create( question: Faker::Lorem.word, answer: Faker::Lorem.word, deck_id: latin.id)}
+titles = Deck.create(name: Faker::Name.title)
+10.times { Card.create( question: Faker::Name.title, answer: Faker::Name.title, deck_id: titles.id)}
+countries = Deck.create(name: Faker::Address.country)
+10.times { Card.create( question: Faker::Address.country, answer: Faker::Address.country, deck_id: countries.id)}
+bs = Deck.create(name: Faker::Company.bs)
+10.times { Card.create( question: Faker::Company.bs, answer: Faker::Company.bs, deck_id: bs.id)}
 
