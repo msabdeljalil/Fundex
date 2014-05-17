e68d3f93ec68ef95b81ee68c93d8ac1545a7916c
msabdeljalil <msabdeljalil@gmail.com>
2014-05-17 09:21:33 -0500

    Renamed Migration create_tables rather than create_users

---
+++ db/migrate/20140516134757_create_tables.rb
@@ -0,0 +1,34 @@
+class CreateTables < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name, :email, :password_hash
+      t.timestamps
+    end
+
+    create_table :decks do |t|
+      t.string :name
+      t.timestamps
+    end
+
+    create_table :rounds do |t|
+      t.integer :user_id
+      t.integer :deck_id
+      t.timestamps
+    end
+
+    create_table :cards do |t|
+      t.string :question, :answer
+      t.integer :deck_id
+      t.timestamps
+    end
+
+    create_table :guesses do |t|
+      t.string :user_input
+      t.integer :correctness
+      t.integer :round_id
+      t.integer :card_id
+      t.timestamps
+    end
+  end
+end
+
